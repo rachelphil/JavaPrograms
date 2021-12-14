5777be932a00c38e0a6fb7f022f517e3fbc98fc6
rachelphil <rachelphilip2027@gmail.com>
2021-12-14 12:36:21 +0530

    Test

--- TestProject/src/package1/Sample.java
+++ TestProject/src/package1/Sample.java
@@ -3,7 +3,7 @@ package package1;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Hello...");
 		System.out.println("Welcome");
 	}
